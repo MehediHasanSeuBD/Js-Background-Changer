3c3c741ab749b2257efe1a9000c85c69473d349d
MehediHasanSeuBD <71432219+MehediHasanSeuBD@users.noreply.github.com>
2022-03-24 20:32:09 +0600

    Add files via upload

---
+++ JS background changer/js/chbg.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $('#greenbtn').click(function(){
+        $('body').removeClass();
+        $('body').addClass('green');
+
+    });
+    $('#yellowbtn').click(function(){
+        $('body').removeClass();
+        $('body').addClass('yellow');
+
+    });
+    $('#orangebtn').click(function(){
+        $('body').removeClass();
+        $('body').addClass('orange');
+
+    });
+
+});
